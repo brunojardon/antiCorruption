85debe69deca41ed3d0613400640ca92d4565721
brunojardon <brunojardon3@gmail.com>
2023-02-05 20:07:59 -0300

    Verificador de integridad.

---
+++ Windows.bat
@@ -0,0 +1,6 @@
+sfc /scannow
+DISM /Online /Cleanup-Image /CheckHealth
+DISM /Online /Cleanup-Image /ScanHealth
+DISM /Online /Cleanup-Image /RestoreHealth
+DISM /Online /Cleanup-Image /RestoreHealth /Source:C:/RepairSource/Windows /LimiAccess
+chkdsk C: /F /R
